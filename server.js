e7e8644fe3efcb80a6228b42eb0e2daf9504e75d
Rounak Paul <90523098+Rounak-Paul@users.noreply.github.com>
2022-07-09 11:28:34 +0530

    1.1

--- server.js
+++ server.js
@@ -19,12 +19,18 @@ app.use(bodyparser.urlencoded({ extended:true}))
 app.set("view engine","ejs")
 
 //load assets
-app.use('css',express.static(path.resolve(__dirname,"assets/css")))
-app.use('img',express.static(path.resolve(__dirname,"assets/img")))
-app.use('js',express.static(path.resolve(__dirname,"assets/js")))
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 
 app.get('/',(req,res)=>{
     res.render('index');
+})
+app.get('/add-user',(req,res)=>{
+    res.render('add_user');
+})
+app.get('/update-user',(req,res)=>{
+    res.render('update_user');
 })
 
 app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)});
